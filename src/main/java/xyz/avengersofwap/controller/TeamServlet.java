e499fdf43afd1203d7b4b8c44a27149a65289233
KaiWang <kwang@mum.edu>
2019-04-22 20:01:19 -0500

    create team model

--- src/main/java/xyz/avengersofwap/controller/TeamServlet.java
+++ src/main/java/xyz/avengersofwap/controller/TeamServlet.java
@@ -1,11 +1,15 @@
 package xyz.avengersofwap.controller;
 
+import xyz.avengersofwap.model.Team;
+import xyz.avengersofwap.util.TeamUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "TeamServlet", urlPatterns = "/team")
 public class TeamServlet extends HttpServlet {
@@ -14,6 +18,8 @@ public class TeamServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Team> teamList = TeamUtil.getAllTeams();
+        request.setAttribute("teamList", teamList);
         request.getRequestDispatcher("WEB-INF/views/Team/team.jsp").forward(request, response);
     }
 }
