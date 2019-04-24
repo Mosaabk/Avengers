b7eb7654a858a881755d5045a1e00094e9b9fff3
zhangziyi <zhangziyiemail@gmail.com>
2019-04-24 11:03:49 -0500

    Merge branch 'master' of https://github.com/Mosaabk/Avengers
           # Conflicts:
           #	src/main/java/xyz/avengersofwap/util/TeamUtil.java
           #	src/main/java/xyz/avengersofwap/util/UserUtil.java
    
           add  new user

--- src/main/webapp/res/js/submit.js
+++ src/main/webapp/res/js/submit.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
                  window.location.href="/user";
             },
             error: function (err) {
-                     alert("success 都是本地数据..");
+                     alert("success");
                 // window.location.href="http://localhost:8080/delete?userid="+userid;
             }
         });
